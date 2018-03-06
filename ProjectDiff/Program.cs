c35d176b431d103fa60fae7587f68edc99e342f0
Pete Gunter <peter.gunter@electricsquare.com>
2018-03-06 15:07:15 +0000

    Program now takes the config path as an argument.

--- ProjectDiff/Program.cs
+++ ProjectDiff/Program.cs
@@ -1,20 +1,29 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace ProjectDiff
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            const string kConfigPath = "C:\\LyFactorGit\\rfactorWrapper\\LyFactor.Orbis\\ProjectDiffConfig.xml";
+            if(args.Length != 1)
+            {
+                Debug.WriteLine("usage: ProjecDif config.xml");
+                return 1;
+            }
+
+            string path = args[0];
 
             // Set the working directory to the one containing the config file
-            var configFileInfo = new FileInfo(kConfigPath);
+            var configFileInfo = new FileInfo(path);
             Directory.SetCurrentDirectory(configFileInfo.Directory.FullName);
 
-            var config = new ConfigLoader(kConfigPath);
+            var config = new ConfigLoader(path);
             var diff = new DiffProcessor(config.Result);
             var diffLogger = new DiffLogger(diff.Result);
+
+            return 0;
         }
     }
 }
